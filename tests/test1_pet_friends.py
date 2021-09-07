02e6ee994de21b32371ef9444520d6b9b168f8ee
vladvladtar <84919501+vladvladtar@users.noreply.github.com>
2021-09-07 15:44:27 +0300

    Update test1_pet_friends.py

--- tests/test1_pet_friends.py
+++ tests/test1_pet_friends.py
@@ -1,1 +1,44 @@
+import api
+from settings import valid_email, valid_password
+import os
+
+pf = api.PetFriends()
+
+
+def test_add_new_pet_without_foto(name='Буся', animal_type='терьер',
+                                     age='4', pet_photo=''):
+    """Проверяем что можно добавить питомца без фото"""
+
+        # Запрашиваем ключ api и сохраняем в переменую auth_key
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    # Добавляем питомца
+    status, result = pf.add_new_pet_without_foto(auth_key, name, animal_type, age)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 200
+    assert result['name'] == name
+
+
+def test_successful_update_self_pet_add_photo(pet_photo='images/masya.jpg'):
+    """Проверяем возможность добавления фото к питомцу"""
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    # Получаем ключ auth_key и список своих питомцев
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Еслди список не пустой, то пробуем обновить его имя, тип и возраст
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_add_photo(auth_key, my_pets['pets'][0]['id'], pet_photo)
+
+        # Проверяем что статус ответа = 200 и имя питомца соответствует заданному
+        assert status == 200
+        assert result['pet_photo'] is not ''  # (выдаёт ошибку)
+
+    else:
+        # если спиок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
+        raise Exception("There is no my pets")
+
 
